222368a6b1688f5db35336f4bb90e1609458c4f4
Volkov Alexander <volkkb@gmail.com>
2016-12-08 15:05:54 +0500

    Megaplan callback script

---
+++ megaplan/logs.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: юзер
+ * Date: 05.12.2016
+ * Time: 10:35
+ */
+
+require './../lib/Log.php';
+
+$log_filename = sprintf('%s/%s', __DIR__, \lib\Log::$log_file);
+$data = file_exists($log_filename) ? file_get_contents($log_filename) : null;
+
+?>
+<meta http-equiv="refresh" content="3">
+<style>
+    table, tr, td { border: 1px solid black; border-collapse: collapse; }
+    table { width: 100%; }
+    td { padding: 1px 10px; }
+
+    tr.log-notice td { background-color: lightblue }
+    tr.log-error td { background-color: lightcoral }
+</style>
+
+<table>
+    <thead>
+        <tr>
+            <td class="col-date">Date</td>
+            <td>Code</td>
+            <td>Message</td>
+            <td>Additional</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?= $data ?>
+    </tbody>
+</table>
