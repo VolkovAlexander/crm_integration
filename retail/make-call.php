98716dfabc79948857ca68532de17f70e868d5e6
Volkov Alexander <volkkb@gmail.com>
2016-12-08 16:20:53 +0500

    remove testing env

--- retail/make-call.php
+++ retail/make-call.php
@@ -3,17 +3,7 @@
 require './../vendor/autoload.php';
 require './../lib/RetailToZadarma.php';
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
 $RetailToZadarma = new \lib\RetailToZadarma();
-
-$RetailToZadarma->Log->notice($ip);
 
 list($code, $phone) = $RetailToZadarma->validateCallbackParams($_GET);
 $RetailToZadarma->makeCallbackToPhone($code, $phone);
