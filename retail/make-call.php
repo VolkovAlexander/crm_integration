1dec996957c5206f7fc745f83a726c87ecab114a
Volkov Alexander <volkkb@gmail.com>
2016-12-08 16:18:17 +0500

    test for incomming ip

--- retail/make-call.php
+++ retail/make-call.php
@@ -3,7 +3,17 @@
 require './../vendor/autoload.php';
 require './../lib/RetailToZadarma.php';
 
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
 $RetailToZadarma = new \lib\RetailToZadarma();
+
+$RetailToZadarma->Log->notice($ip);
 
 list($code, $phone) = $RetailToZadarma->validateCallbackParams($_GET);
 $RetailToZadarma->makeCallbackToPhone($code, $phone);
