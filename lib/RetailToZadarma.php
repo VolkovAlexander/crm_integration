12b2f2515c5b8c293ebd65d68335cc4cdfa6b641
Volkov Alexander <volkkb@gmail.com>
2016-12-12 15:22:15 +0500

    mysql integration

--- lib/RetailToZadarma.php
+++ lib/RetailToZadarma.php
@@ -425,7 +425,7 @@ class RetailToZadarma extends AbstractZadarmaIntegration
 
         if (!empty($calls_to_upload)) {
             foreach ($calls_to_upload as $Call) {
-                $this->Log->notice('new call', $Call);
+                $this->Log->notice('new call', print_r($Call, true));
 
                 $pbx_call_id = $Call->call_id;
                 $call_record_link = $this->getCallRecord(null, $pbx_call_id);
