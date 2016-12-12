d282d8bed998da56ab1112b6e550f0b7fb5b9901
Volkov Alexander <volkkb@gmail.com>
2016-12-12 16:16:09 +0500

    mysql integration

--- retail/make-call.php
+++ retail/make-call.php
@@ -6,4 +6,5 @@ require './../lib/RetailToZadarma.php';
 $RetailToZadarma = new \lib\RetailToZadarma();
 
 list($code, $phone) = $RetailToZadarma->validateCallbackParams($_GET);
+error_log('CALLBACK TO PHONE: ' . $code . ' ' . $phone);
 $RetailToZadarma->makeCallbackToPhone($code, $phone);
