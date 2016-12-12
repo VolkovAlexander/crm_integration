67a4af3a4d8c17e03f1893bb85b1dbea1f536c5b
Volkov Alexander <volkkb@gmail.com>
2016-12-12 13:05:31 +0500

    mysql integration

--- retail/events.php
+++ retail/events.php
@@ -36,7 +36,7 @@ $data = $RetailToZadarma->Mysql->table('retail')->select('*')->get();
                 <tr>
                     <td><?= date('d-M-y H:i', $Row->created_at) ?></td>
                     <td><?= $Row->status ?></td>
-                    <td><pre><?= $Row->data ?></pre></td>
+                    <td><pre><?= print_r(json_decode($Row->data, true), true) ?></pre></td>
                 </tr>
             <?php endforeach; ?>
         <?php endif; ?>
