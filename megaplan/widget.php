6107030d699c964ced5bf556755e22d992d55eff
Volkov Alexander <volkkb@gmail.com>
2016-12-14 18:05:25 +0500

    megaplan widgets

---
+++ megaplan/widget.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Volkov Alexander
+ */
+?>
+
+<div class="telephony-call-block">
+    <div class="telephony-call-block__header"><h4>Звонит клиент <a href="/crm/1000000/card/" target="_blank">Синицын
+                Олег Геннадьевич</a></h4>
+        <hr>
+    </div>
+    <div class="telephony-call-block__body"><b>Количество заказов: </b> 123<br><b>Последний звонок: </b> 23.11.2016
+        11:00
+        <hr>
+    </div>
+    <div class="telephony-call-block__footer">
+        <button class="btn btn-primary pull-left">+ Создать новый заказ</button>
+        <button class="btn btn-default pull-right close-new-call-notification">Закрыть</button>
+    </div>
+</div>
