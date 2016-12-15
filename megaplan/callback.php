ca4a65c5a1c4e811041dc127089a9441570b3b40
Volkov Alexander <volkkb@gmail.com>
2016-12-15 13:51:05 +0500

    megaplan build

--- megaplan/callback.php
+++ megaplan/callback.php
@@ -10,4 +10,6 @@ $logClient = new \lib\MegaplanToZadarma();
 
 if(!empty($_POST)) {
     $logClient->Log->notice('New request', $_POST);
+} else {
+    $logClient->Log->error('New request with empty data');
 }
